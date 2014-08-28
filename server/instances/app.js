51ac4bdfd9db34550fd600bff03a6efbccc14e77
Aleks Totic <a@totic.org>
2014-08-28 09:35:58 -0700

    Made it all work in Firefox/Safari.

--- server/instances/app.js
+++ server/instances/app.js
@@ -21,6 +21,13 @@ var homepath = path.resolve(__dirname, '../..');
 app.set('views', path.join(homepath, 'browser/views'));
 app.set('view engine', 'jade');
 
+// enable CORS on development for testing
+if (app.get('env') === 'development') {
+  app.use(function(req, res, next) {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      return next();
+  });
+}
 app.use(favicon(homepath + '/browser/favicon.ico'));
 app.use(logger('dev'));
 app.use(cookieParser("WEJNSD<MVCSDKJF"));
@@ -38,9 +45,9 @@ app.get('/polymer', function stats(req, res, next) {
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -48,22 +55,22 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: err
+      });
+  });
 }
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+      message: err.message,
+      error: {}
+  });
 });
 
 module.exports = app;
